8a45edc05d1dfb45b671f2b90392058d09a0b621
Raghu-7 <45433335+Raghu-7@users.noreply.github.com>
2019-04-03 23:00:52 +0530

    Add files via upload

---
+++ ShinyApp/server.R
@@ -0,0 +1,34 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+suppressPackageStartupMessages(c(
+  library(shinythemes),
+  library(shiny),
+  library(tm),
+  library(stringr),
+  library(markdown),
+  library(stylo)))
+
+source("./predictor.R")
+final4Data <- readRDS(file="./data/final4Data.RData")
+final3Data <- readRDS(file="./data/final3Data.RData")
+final2Data <- readRDS(file="./data/final2Data.RData")
+
+
+shinyServer(function(input, output) {
+  
+  wordPrediction <- reactive({
+    text <- input$text
+    textInput <- cleanInput(text)
+    wordCount <- length(textInput)
+    wordPrediction <- nextWordPrediction(wordCount,textInput)})
+  
+  output$predictedWord <- renderPrint(wordPrediction())
+  output$enteredWords <- renderText({ input$text }, quoted = FALSE)
+})
